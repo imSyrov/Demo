10dd5fef0e1587275c67645138da23a9c36b846e
Ilya Syrov <im.syrov.developer@yandex.ru>
2023-10-09 19:13:25 +0400

    cp: add cherry-pick demo file

---
+++ DemoSwift/DemoSwift/CherryPickDemo.swift
@@ -0,0 +1,13 @@
+//
+//  CherryPickDemo.swift
+//  DemoSwift
+//
+//  Created by Ilya Syrov on 09.10.2023.
+//
+
+// Cherry-pick git comand is here
+import Foundation
+
+fileprivate func foo() -> String {
+    "Cherry-pick demo"
+}
