1b301dd7485c1f9bb37f59c2951b0977e5140689
Ilya Syrov <im.syrov.developer@yandex.ru>
2023-10-09 19:16:16 +0400

    rb: add rebase demo file

---
+++ DemoSwift/DemoSwift/RebaseDemo.swift
@@ -0,0 +1,15 @@
+//
+//  RebaseDemo.swift
+//  DemoSwift
+//
+//  Created by Ilya Syrov on 09.10.2023.
+//
+
+//Rebase git command is here
+
+import Foundation
+
+
+fileprivate func foo() -> String {
+    "Rebase demo"
+}
